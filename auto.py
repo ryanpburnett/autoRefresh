7d0ade07be7dff233d193a0c88f452ee2f111134
ryan <ryanpburnett@yahoo.com>
2024-01-14 16:47:03 -0600

    got rid of commented out old code

--- auto.py
+++ auto.py
@@ -1,24 +1,3 @@
-# import keyboard
-# import time
-# from datetime import timedelta
-
-# sec = 15
-# refresh = 0
-# pause = 0
-
-# while True:
-#     if keyboard.is_pressed("p"):
-#         keyboard.press_and_release("ctrl+r")
-#         print("refresh\t", timedelta(seconds=refresh))
-#         refresh += sec
-#     else: 
-#         print("pause\t", timedelta(seconds=pause))
-#         pause += sec
-
-#     time.sleep(sec)
-
-# ----ALTERNATE VERSION----
-
 import keyboard
 import time
 from datetime import timedelta
